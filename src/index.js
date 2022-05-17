45754ce151c19e177ab41e0db7410f20047478db
Carlos <cdbm342@gmail.com>
2022-05-17 19:46:41 -0400

    Se crea la carpeta hooks el UseCounter y el CounterWithCustomHook

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './components/o1-useState/CounterApp';
+//import { CounterApp } from './components/o1-useState/CounterApp';
+import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
 //import { HooksApp } from './HooksApp';
 
 
@@ -8,7 +9,7 @@ import { CounterApp } from './components/o1-useState/CounterApp';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <CounterApp />
+    <CounterWithCustomHook />
 
 );
 
