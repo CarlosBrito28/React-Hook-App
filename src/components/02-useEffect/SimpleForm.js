e906a0452115871bbdf9954ee4ea7d47676f2166
Carlos <cdbm342@gmail.com>
2022-05-19 10:38:38 -0400

    FormCustomHook

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import './effects.css';
 import { Message } from './Message';
+import './effects.css';
+
 export const SimpleForm = () => {
+    
 const [formState, setFormState] = useState({
     name:'',
     email:''
 });
 const{ name, email} = formState;
 
-// useEffect( () => {
-//     console.log('hey!')
-// }, [] );
+useEffect( () => {
+    //console.log('hey!')
+}, [] );
 
-// useEffect( () => {
-//     console.log('formState Cambio!')
-// }, [formState] );
+useEffect( () => {
+   // console.log('formState Cambio!')
+}, [formState] );
 
-// useEffect( () => {
-//     console.log('email cambio!')
-// }, [e] );
+useEffect( () => {
+    //console.log('email cambio!')
+}, [e] );
 
 const handleImputChange = ({target}) =>{
     setFormState({
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { SimpleForm } from './components/02-useEffect/SimpleForm';
+
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
 //import { CounterApp } from './components/o1-useState/CounterApp';
 //import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
 //import { HooksApp } from './HooksApp';
-
-
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <SimpleForm />
+    <FormWithCustomHook />
 
 );
 
