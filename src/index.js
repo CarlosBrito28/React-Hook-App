0c9eb93a3e5b0982019242be364f153338d25fa9
Carlos <cdbm342@gmail.com>
2022-06-01 17:38:59 -0400

    UseReducer primeros pasos con este hook

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-// //import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// //import { CounterApp } from './components/o1-useState/CounterApp';
-// //import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
-// //import { HooksApp } from './HooksApp';
-// //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-// //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-// //import { FocusScreen } from './components/04-useRef/FocusScreen';
-// //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-// //import { Layout } from './components/05-useLayoutEffect/Layout';
-// //import { Memorize } from './components/06-memos/Memorize';
-// //import { MemoHook } from './components/06-memos/MemoHook';
-// import { CallBackHook } from './components/06-memos/CallBackHook';
+//import { SimpleForm } from './components/02-useEffect/SimpleForm';
+//import { CounterApp } from './components/o1-useState/CounterApp';
+//import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
+//import { HooksApp } from './HooksApp';
+//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+//import { FocusScreen } from './components/04-useRef/FocusScreen';
+//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+//import { Layout } from './components/05-useLayoutEffect/Layout';
+//import { Memorize } from './components/06-memos/Memorize';
+//import { MemoHook } from './components/06-memos/MemoHook';
+//import { CallBackHook } from './components/06-memos/CallBackHook';
+import { TodoApp } from './components/08-useReducer/TodoApp';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 
-//     <CallBackHook />
+    <TodoApp />
 
-// );
+);
 
-import './components/08-useReducer/intro-reducer';
+//import './components/08-useReducer/intro-reducer';
