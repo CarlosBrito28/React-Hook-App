95331ad4213899e217744b5a71db3c6960fa078b
Carlos <cdbm342@gmail.com>
2022-06-01 17:03:39 -0400

    intro al Reducer

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 
-//import { SimpleForm } from './components/02-useEffect/SimpleForm';
-//import { CounterApp } from './components/o1-useState/CounterApp';
-//import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
-//import { HooksApp } from './HooksApp';
-//import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-//import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
-//import { FocusScreen } from './components/04-useRef/FocusScreen';
-//import { RealExampleRef } from './components/04-useRef/RealExampleRef';
-//import { Layout } from './components/05-useLayoutEffect/Layout';
-//import { Memorize } from './components/06-memos/Memorize';
-//import { MemoHook } from './components/06-memos/MemoHook';
-import { CallBackHook } from './components/06-memos/CallBackHook';
+// //import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// //import { CounterApp } from './components/o1-useState/CounterApp';
+// //import { CounterWithCustomHook } from './components/o1-useState/CounterWithCustomHook';
+// //import { HooksApp } from './HooksApp';
+// //import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// //import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// //import { FocusScreen } from './components/04-useRef/FocusScreen';
+// //import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// //import { Layout } from './components/05-useLayoutEffect/Layout';
+// //import { Memorize } from './components/06-memos/Memorize';
+// //import { MemoHook } from './components/06-memos/MemoHook';
+// import { CallBackHook } from './components/06-memos/CallBackHook';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 
-    <CallBackHook />
+//     <CallBackHook />
 
-);
+// );
 
+import './components/08-useReducer/intro-reducer';
