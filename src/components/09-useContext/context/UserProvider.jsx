f0e936d72d3f388061c094981317efdd8601b3de
Carlos <cdbm342@gmail.com>
2022-06-12 20:30:59 -0400

    Use context

--- src/components/09-useContext/context/UserProvider.jsx
+++ src/components/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import { UserContext } from "./UserContext"
 
-const user ={
-    id: 123, 
-    name: 'Carlos Brito',
-    email: 'Cdbm@QLQ.com'
-}
+// const user ={
+//     id: 123, 
+//     name: 'Carlos Brito',
+//     email: 'Cdbm@QLQ.com'
+// }
 export const UserProvider = ({ children }) => {
+  const [user, setUser]   = useState();
+
   return (
-    <UserContext.Provider value={{ Hola: 'Mundo', user: user}}>
-        { children }
-    </UserContext.Provider>
+    // <UserContext.Provider value={{ Hola: 'Mundo', user: user}}>
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider >
   )
 }
